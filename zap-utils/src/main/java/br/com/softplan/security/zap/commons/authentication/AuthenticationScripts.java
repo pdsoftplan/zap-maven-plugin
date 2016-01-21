66d4a583cb63f6dc3e23e36f954d65d71398c164
Thiago Porciúncula <thiago.porciuncula@softplan.com.br>
2016-01-21 14:16:01 -0200

    Add AuthenticationScripts

---
+++ zap-utils/src/main/java/br/com/softplan/security/zap/commons/authentication/AuthenticationScripts.java
@@ -0,0 +1,8 @@
+package br.com.softplan.security.zap.commons.authentication;
+
+public class AuthenticationScripts {
+
+	public static final String RELATIVE_PATH = "/scripts/";
+	public static final String EXTENSION = ".js";
+	
+}
